363a9b5600239d38de93c4c035ce17c9f5789fd7
ktinson <monikai.tinson@verizon.com>
2024-09-23 09:47:18 -0500

    Updated POST and PUT request

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const db = require("../db/connection");
 const seed = require("../seedData")
 //TODO: Create your GET Request Route Below: 
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended: true}))
 app.get('/restaurants', async (req, res) => {
     let result = await Restaurant.findAll()
     res.json(result)
@@ -14,13 +14,23 @@ app.get('/restaurants/:id', async (req, res) => {
     let result = await Restaurant.findByPk(req.params.id)
     res.json(result)
 })
-app.post('/restaurants/', async (req, res) => {
+app.post('/restaurants/test', async (req, res) => {
     let result = await Restaurant.create({name: 'AppleTest',
         location: 'Test',
         cuisine: 'Fasttest'})
     res.json(result)
+    
+})
+app.post('/restaurants/', async (req, res) => {
+    let result = await Restaurant.create(req.body)
+    res.json(result)
 })
 app.put('/restaurants/:id', async (req, res) => {
+
+    let result = await Restaurant.update(req.body,{where:{id :req.params.id}})
+    res.json(result)
+})
+app.put('/restaurants/test/:id', async (req, res) => {
 
     let result = await Restaurant.update({name: 'AppleTest2'},{where:{id :req.params.id}})
     res.json(result)
